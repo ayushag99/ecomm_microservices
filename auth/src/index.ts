972f4181d44c4d23f95f179f424dc9b784b412b1
Ayush Agrawal <ayush@Ayushs-MacBook-Air.local>
2022-04-13 10:08:31 +0530

    refactor: rafactoring the auth code for proper folder structure

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,13 +2,19 @@ const PORT = 3000;
 import express from 'express';
 import { json } from 'body-parser'
 
+import {currentUserRouter} from './routes/current-user'
+import {signInRouter} from './routes/signin'
+import {signOutRouter} from './routes/signout'
+import {signUpRouter} from './routes/signup'
+
 const app = express();
 
 app.use(json())
 
-app.get("/",(req,res)=>{
-    res.status(200).send("Authintication service is healthy and running");
-})
+app.use(currentUserRouter)
+app.use(signInRouter)
+app.use(signOutRouter)
+app.use(signUpRouter)
 
 app.get("/api/users/currentuser", (req,res)=>{
     res.send("hi there!");
