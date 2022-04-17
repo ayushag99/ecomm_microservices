c7114347078a20e210abb3d7562900d6be44148a
Ayush Agrawal <ayush.ashuvaish@gmail.com>
2022-04-17 17:59:12 +0530

    [auth] add: User logic to current user router

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,23 @@
-import express from 'express';
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req,res) => {
-    res.send("TEST")
-})
+// Check user is logged in or not, current user
+router.get("/api/users/currentuser", (req: Request, res: Response) => {
+  // if (!req.session || !req.session.jwt) {
+  if (!req.session?.jwt) {
+      console.log("User not logged in ")
+    return res.send({ currentUser: null });
+  }
 
-export {router as currentUserRouter}; 
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (err) {
+    //   JWT verification fails
+    res.send({ currentUser: null });
+  }
+});
+
+export { router as currentUserRouter };
