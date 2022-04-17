d18203f8e04939444a4a52d2d9355e6641faafe9
Ayush Agrawal <ayush.ashuvaish@gmail.com>
2022-04-17 18:01:47 +0530

    [auth] add: logic to signout user router

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,9 +1,11 @@
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
 
-router.post('/api/users/signout', (req,res) => {
-    res.send("TEST")
-})
+router.post("/api/users/signout", (req, res) => {
+  // Ask user to clear cookie
+  req.session = null;
+  res.send({});
+});
 
-export {router as signOutRouter}; 
+export { router as signOutRouter };
