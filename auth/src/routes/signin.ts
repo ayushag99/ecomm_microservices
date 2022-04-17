6dacb395b32f4388e91e66253e1e2a347d4cfe85
Ayush Agrawal <ayush.ashuvaish@gmail.com>
2022-04-17 17:40:42 +0530

    [auth] add: User Sign in logic

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { BadRequestError } from "../errors/bad-request-error";
 import { RequestValidationError } from "../errors/request-validaion-error";
 import { validateRequest } from "../middlewares/validate-request";
+import { User } from "../Models/users";
+import { Password } from "../services/password";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -15,8 +19,44 @@ router.post(
       .withMessage("You must supply a password"),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    // User not found
+    if (!existingUser) {
+      throw new BadRequestError("Invalid Credentials");
+    }
+
+    // User exists
+    // Compare password
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordsMatch) {
+      throw new BadRequestError("Invalid Credentials");
+    }
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      // JWT_KEY was already checked in index.ts at start up
+      // Thus the exclamation mark
+      process.env.JWT_KEY!
+    );
+
+    // Store it on session object
+
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
   }
 );
 
