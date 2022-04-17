5b1d22365aba9fd363c71d06a93456d314c637b7
Ayush Agrawal <ayush.ashuvaish@gmail.com>
2022-04-17 17:08:06 +0530

    [auth] add: JWT and JWT_KEY env variable in k8s pod

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser'
 import mongoose from 'mongoose';
+import cookieSession  from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user'
 import { signInRouter } from './routes/signin'
@@ -13,7 +14,19 @@ import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 
+// Traffic is beig proxied to this app by nginx
+// To inform express that it is behind the proxt and 
+// To make express trust the data that is coming from a proxy
+app.set('trust proxy', true);
+
+
 app.use(json())
+app.use(
+    cookieSession({
+        signed: false, 
+        secure: true
+    })
+)
 
 app.use(currentUserRouter)
 app.use(signInRouter)
@@ -33,6 +46,9 @@ app.use(errorHandler)
 // 
 
 const start = async () => {
+    if (!process.env.JWT_KEY){
+        throw new Error("JWT_KEY not found")
+    }
     try {
         await mongoose.connect('mongodb://auth-mongo-srv:27017/ticketing_auth')
         console.log("Connected to Database");
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,8 +2,9 @@ import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { User } from '../Models/users';
 import { RequestValidationError } from '../errors/request-validaion-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
 import { BadRequestError } from '../errors/bad-request-error';
+
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -25,6 +26,23 @@ router.post('/api/users/signup', [
 
     const user = User.build({email, password})
     await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email
+    },
+    // JWT_KEY was already checked in index.ts at start up 
+    // Thus the exclamation mark 
+    process.env.JWT_KEY!
+    );
+
+
+    // Store it on session object
+
+    req.session = {
+        jwt: userJwt,
+    }
 
     res.status(201).send(user)
 })
