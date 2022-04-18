9f4bec19ce550f53daef7dc09263fdd9aed8b98f
Ayush Agrawal <ayush.ashuvaish@gmail.com>
2022-04-18 11:36:24 +0530

    [client] add: custom hook for making api calls and error handling

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,22 +1,21 @@
 import axios from 'axios';
 import { useState } from 'react';
+import useRequest from '../../hooks/use-request';
 
 const Signup = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('')
-    const [errors, setErrors] = useState([])
+    const { doRequest, errors } = useRequest({
+        url: '/api/users/signup',
+        method: 'post',
+        body: {
+            email, password
+        }
+    })
 
     const onSubmit = async (event) => {
         event.preventDefault();
-        try {
-
-            const resposne = await axios.post('/api/users/signup', {
-                email, password
-            })
-            console.log(resposne)
-        } catch (err) {
-            setErrors(err.response.data.errors)
-        }
+        doRequest();
     }
 
     return (
@@ -30,13 +29,7 @@ const Signup = () => {
                 <label>Password</label>
                 <input value={password} type="password" className="form-control" onChange={e => setPassword(e.target.value)} />
             </div>
-            {errors.length>0 && <div className='alert alert-danger'>
-                <h4>Oooops.....</h4>
-                <ul className='my-0'>
-
-                    {errors.map(err => <li key={err.message}>{err.message}</li>)}
-                </ul>
-            </div>}
+            {errors}
             <button className="btn btn-primary">Sign Up</button>
         </form>
     )
