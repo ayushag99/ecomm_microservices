aaaf6611ffe252aeadbb7cba5f798093add44f13
Ayush Agrawal <ayush.ashuvaish@gmail.com>
2022-04-21 09:21:20 +0530

    [ticket-ms] add: more TDD

--- ticket-ms/src/routes/new.ts
+++ ticket-ms/src/routes/new.ts
@@ -1,10 +1,22 @@
 import express, { Request, Response } from "express";
-import { requireAuth } from "@aa-ticketing/common";
+import { requireAuth, validateRequest } from "@aa-ticketing/common";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/tickets", requireAuth, (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
+router.post(
+  "/api/tickets",
+  requireAuth,
+  [
+    body("title").not().isEmpty().withMessage("Title is required"),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price must be greater than zero"),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.sendStatus(200);
+  }
+);
 
 export { router as createTickerRouter };
