166fdf06e50e7917c595886b3d3f5af7c67fa5a6
Ayush Agrawal <ayush.ashuvaish@gmail.com>
2022-04-21 10:39:23 +0530

    [ticket-ms] test: Ticket Creation

--- ticket-ms/src/routes/__test__/new.test.ts
+++ ticket-ms/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
 
 it("has a route handler listening to /api/tickets for post request", async () => {
   const response = await request(app).post("/api/tickets").send({});
@@ -55,12 +56,21 @@ it("returns an error if an invalid price is provided", async () => {
 });
 
 it("creates ticket with valid input", async () => {
-  await request(app)
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+
+  const ticket = {
+    title: "Title",
+    price: 200,
+  };
+  const response = await request(app)
     .post("/api/tickets")
     .set("Cookie", global.signin())
-    .send({
-      title: "Title",
-      price: 200,
-    })
+    .send(ticket)
     .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].price).toEqual(ticket.price);
+  expect(tickets[0].title).toEqual(ticket.title);
 });
