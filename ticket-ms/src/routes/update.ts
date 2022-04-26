cb280f2b6be30e9420e7fe5c1cc55bc3dd8d9068
Ayush Agrawal <ayush.ashuvaish@gmail.com>
2022-04-26 07:47:42 +0530

    add: logic for successfull ticket creation

--- ticket-ms/src/routes/update.ts
+++ ticket-ms/src/routes/update.ts
@@ -14,18 +14,30 @@ const router = express.Router();
 router.put(
   "/api/tickets/:id",
   requireAuth,
+  [
+    body("title").not().isEmpty().withMessage("Title is required"),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price should be greater than 0"),
+  ],
+  validateRequest,
   async (req: Request, res: Response) => {
     const ticket = await Ticket.findById(req.params.id);
-    console.log(ticket)
+    console.log(ticket);
     if (!ticket) {
-      console.log("TICKET", ticket)
+      console.log("TICKET", ticket);
       throw new NotFoundError();
     }
-    console.log("Found Ticket", ticket)
+    console.log("Found Ticket", ticket);
     if (ticket.userId !== req.currentUser!.id) {
       throw new NotAuthorisedError();
     }
 
+    ticket.set({
+      title: req.body.title,
+      price: req.body.price,
+    });
+    await ticket.save();
     res.send(ticket);
   }
 );
